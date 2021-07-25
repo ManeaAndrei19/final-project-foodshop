2499a248de9c44c86c2059851ccd0a9a6c6ea677
ManeaAndrei19 <maneaandrei19@yahoo.com>
2021-07-25 12:55:20 +0300

    Add Initial Structure for Final Project

---
+++ src/test/java/com/maneaandrei/sda/foodshop/FoodshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maneaandrei.sda.foodshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
