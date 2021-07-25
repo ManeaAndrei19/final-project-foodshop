86d1bb90ac84c6b2b4fa82ed73149206caa4367b
ManeaAndrei19 <maneaandrei19@yahoo.com>
2021-07-25 13:34:57 +0300

    Add Database Connection configuration

--- src/main/java/com/maneaandrei/sda/foodshop/FoodShopApplication.java
+++ src/main/java/com/maneaandrei/sda/foodshop/FoodShopApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FoodshopApplication {
+public class FoodShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FoodshopApplication.class, args);
+		SpringApplication.run(FoodShopApplication.class, args);
 	}
 
 }
